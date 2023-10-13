10ca713ac63714cca2b125a3db51cfc56e540efc
Willmuseve@ <willmuseve@gmail.com>
2023-10-13 07:55:48 -0700

    Updated the file storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,8 @@
 import os
 import json
 from models.base_model import BaseModel
+from models.user import User
+
 
 class FileStorage:
     """A class that serializes instances to a JSON file and deserializes
@@ -13,31 +15,37 @@ class FileStorage:
 
     def all(self):
         """ returns the dictionary __objects """
-        return FileStorage.self.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """serializes __objects to the JSON file (path: __file_path)
+        Saves user object along with other objects
+        """
         objects_dict = {}
-        for key, obj in FileStorage.__objects.items():
+        for key, obj in self.__objects.items():
             objects_dict[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(objects_dict, file)
 
     def reload(self):
         """ deserializes the JSON file to __objects"""
-        if os.path.exists(FileStorage.__file_path):
+        if os.path.exists(self.__file_path):
             try:
-                with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+                with open(self.__file_path, 'r', encoding='utf-8') as file:
                     objects_dict = json.load(file)
                     for key, obj_dict in objects_dict.items():
-                        class_name, obj_id = key.split('.')
-                        obj = eval(class_name)(**obj_dict)
-                        FileStorage.__objects[key] = obj
+                        if '__class__' in obj_dict and obj_dict['__class__'] =\
+                                = "User":
+                            obj = User(**obj_id)
+                        else:
+                            class_name, obj_id = key.split('.')
+                            obj = eval(class_name)(**obj_dict)
+                            self.__objects[key] = obj
             except FileNotFoundError:
                 pass
 
