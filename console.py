0cd5d8c9780a8c725ea01a20be2dbd984263d8d5
Willmuseve@ <willmuseve@gmail.com>
2023-10-13 22:41:12 -0700

    Updated the console.py to allow show, create, destroy, update and all with all classes created previously.

--- console.py
+++ console.py
@@ -5,17 +5,26 @@ import models
 from models import *
 from models.basemodel import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.places import Places
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """class definition which creates an interactive section with the prompt"""
+    """class that creates an interactive session with the prompt"""
 
     prompt = "(hbnb) "
 
     classes = {
-            'BaseModel' : BaseModel,
-            'User' : User
-            }
+            'BaseModel': BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review}
 
     def do_quit(self, param):
         """Quits the program"""
@@ -94,8 +103,8 @@ class HBNBCommand(cmd.Cmd):
         elif param[0] not in self.classes:
             print("** class doesn't exist **")
         else:
-            print([str(obj) for key, obj in objects.items() if\
-                 key.split('.')[0] == param[0]])
+            print([str(obj) for key, obj in objects.items() if
+                key.split('.')[0] == param[0]])
 
     def do_update(self, param):
         """Updates an instance based on the class name and id by adding or
