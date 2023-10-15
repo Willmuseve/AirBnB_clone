7261f94d637b72e887338d0b79fdfb07e6617095
Willmuseve@ <willmuseve@gmail.com>
2023-10-14 21:57:07 -0700

    correction

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,8 @@
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
 
+from models.engine.file_storage import FileStorage
+"""
+Creates a new instance of FileStorage
+"""
 storage = FileStorage()
 storage.reload()
