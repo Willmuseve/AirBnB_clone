bd92383b3385b2d0b0e8fe5d2faabeba0658589c
Willmuseve@ <willmuseve@gmail.com>
2023-10-14 22:03:38 -0700

    correction

--- models/user.py
+++ models/user.py
@@ -7,8 +7,7 @@ class User(BaseModel):
     """A class user that inherits from the base model
     and has email, password, first_name and last_name
     as public attributes"""
-
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
